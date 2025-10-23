4af7d132fab59d5c895bc440a3dd95486d7094e9
Londiwe123 <anelemvubu90@gmail.com>
2025-10-23 14:28:40 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").addEventListener("click", function() {
+  const message = document.getElementById("message");
+  message.textContent = "Hello, Cloud Developer! 🚀";
+  message.style.color = "#0077b6";
+});
